d3dfa099798f21b468d84fd64ab51497b4e5ec62
NazarDobriy <51053478+NazarDobriy@users.noreply.github.com>
2023-05-31 18:59:18 +0300

    Add files via upload

---
+++ robotic_platform.ino
@@ -0,0 +1,70 @@
+#include <Ultrasonic.h>
+#include <SoftwareSerial.h>
+
+char cmd = ' ';
+Ultrasonic ultrasonic (11, 12);
+SoftwareSerial bluetooth (0, 1);
+
+void setup() {
+  bluetooth.begin(9600);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  analogWrite(5, 255);
+  analogWrite(10, 255);
+}
+
+void loop() {
+  if (bluetooth.available() && ultrasonic.read() > 10) {
+    cmd = bluetooth.read();
+    if (cmd == 'F') {
+      forward();
+    } else if (cmd == 'B') {
+      back();
+    } else if (cmd == 'L') {
+      left();
+    } else if (cmd == 'R') {
+      right();
+    } else if (cmd == 'S') {
+      stop();
+    }
+  } else {
+    stop();
+  }
+}
+
+void forward() {
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+}
+
+void back() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+}
+
+void left() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+}
+
+void right() {
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+}
+
+void stop() {
+  analogWrite(5, 0);
+  analogWrite(10, 0);
+}
